0822809970c04c827fba4937ab7c728976916595
Superboi777 <68790401+Superboi07@users.noreply.github.com>
2026-07-08 10:40:36 -0400

    Update Test.java

--- JavaFX/src/main/java/com/summertech2026/Test.java
+++ JavaFX/src/main/java/com/summertech2026/Test.java
@@ -1,6 +1,7 @@
 package com.summertech2026;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,8 +11,14 @@ import org.json.JSONObject;
 public class Test {
     public static void main(String args[]) throws IOException{
         File file = new File("src/main/resources/com/summertech2026/test.json");
-        String content = new String(Files.readAllBytes(Paths.get(file.toURI())));
-        JSONObject jo = new JSONObject(content);
+        JSONObject jo = new JSONObject(new String(Files.readAllBytes(Paths.get(file.toURI()))));
+        
         System.out.println(jo.get("test"));
+
+        jo.put("new variable", 10);
+    
+        FileWriter writer = new FileWriter(file);
+        writer.write(jo.toString());
+        writer.close();
     }
 }
